cf866df10ef1ff9c76df2600d02d7c7d4fb35d6c
dev-khaiduy <103346903+dev-khaiduy@users.noreply.github.com>
2023-07-03 17:04:12 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyBMomRwU1cCq_IjNjQcnyffpwU1EdlcP5Y',
+    messagingSenderId: '237028699118',
+    appId: '1:237028699118:web:8d739cd5eaa9fd75627cff',
+    projectId: 'mobilestore-2beca',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96BSUH1PDDQFBSPLEV97HGG26AH0G',
+    siteid: '6b65634a2ec2038632afb6fb7b18bc6b'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
