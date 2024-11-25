cbf050e5751e04b758051ba5b524b303dac2100b
Khaile <khai.le@akagidital.vn>
2024-11-25 16:42:46 +0700

    new

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ var config = {
     messagingSenderId: '237028699118',
     appId: '1:237028699118:web:8d739cd5eaa9fd75627cff',
     projectId: 'mobilestore-2beca',
-    user_key: 'ADGMOT35CHFLVDHBJNIG50K96BSUH1PDDQFBSPLEV97HGG26AH0G',
-    siteid: '6b65634a2ec2038632afb6fb7b18bc6b'
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96A9F4B0JDMK47ULMNUBFRKVO0GG0',
+    siteid: '170c8c813139d4260c4273a5f8f46b62'
 };
 importScripts('//cdnt.netcoresmartech.com/swv4.js');
